1d9fdcb2905f6d339643e0080fa78839fc4a775c
lijinlong <1029171429@qq.com>
2016-07-09 19:38:03 +0800

    归并排序, 插入排序算法

---
+++ src/main/java/cn/tk/java/algorithm/InsertSort.java
@@ -0,0 +1,46 @@
+package cn.tk.java.algorithm;
+
+import org.junit.Test;
+
+public class InsertSort {
+	
+	/**
+	 * @description:按从小到大排序
+	 */
+	public void insertSort(int[] array)
+	{
+		if(array == null || array.length < 1)
+		{
+			System.out.println("请输入正确整数序列!");
+			return;
+		}
+		
+		/**
+		 * @description: 1~(i-1) 均为有序数组, 插入 array[i], 变成 1~i 均为有序数组
+		 */
+		for(int i=0; i<array.length; i++)
+		{
+			int key = array[i];
+			int position = i;
+			for(int j =i-1; j>=0; j--)//从后向前遍历
+			{
+				if(array[j] > key)
+				{
+					array[j+1] = array[j];
+					position --;
+				}
+			}
+			array[position] = key;
+		}
+	}
+	
+	@Test
+	public void testInsertSort()
+	{
+		int[] array = {5, 12, 12, 1, 23, 34, 5, 11};
+		insertSort(array);
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+}
---
+++ src/main/java/cn/tk/java/algorithm/MergeSort.java
@@ -0,0 +1,69 @@
+package cn.tk.java.algorithm;
+
+import org.junit.Test;
+
+public class MergeSort {
+	
+	/**
+	 * @description:分治法编写归并排序, 核心是 "合并" 方法
+	 * 分解, 递归, 合并
+	 */
+	private void mergeSort(int[] data, int left, int right) {
+		if (left < right) {
+			int middle = (left + right) / 2;
+			mergeSort(data, left, middle);
+			mergeSort(data, middle+1, right);
+			merge(data, left, middle, right);
+			print(data);
+		}
+	}
+	
+	/**
+	 * @description: 合并两个有序序列
+	 * left, middle, right: 数组边界点
+	 */
+	public void merge(int[] data, int left, int middle, int right)
+	{
+		int[] tempArray = new int[data.length];
+		int leftPos = left;
+		int tempPos = left;
+		int midPos = middle + 1;
+		while(leftPos<=middle && midPos<=right)
+		{
+			if (data[leftPos]<data[midPos]) {
+				tempArray[tempPos++] = data[leftPos++];
+			}
+			else {
+				tempArray[tempPos++] = data[midPos++];
+			}
+		}
+		
+		while(leftPos<=middle)
+		{
+			tempArray[tempPos++] = data[leftPos++];
+		}
+		while(midPos<=right)
+		{
+			tempArray[tempPos++] = data[midPos++];
+		}
+		
+		while(left<=right)
+		{
+			data[left] = tempArray[left++];
+		}
+	}
+	
+	@Test
+	public void testInsertSort()
+	{
+		int[] array = {5, 12, 12, 1, 23, 34, 5, 11};
+		mergeSort(array, 0, array.length-1);
+	}
+
+	private void print(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + "\t");
+		}
+		System.out.println();
+	}
+}
