92df68c74e5f57aeff36aedeb15163aa0e870eca
lijinlong <1029171429@qq.com>
2016-08-31 16:31:26 +0800

    四舍五入算法

--- src/main/java/cn/tk/java/util/ValueConvert.java
+++ src/main/java/cn/tk/java/util/ValueConvert.java
@@ -2,10 +2,11 @@ package cn.tk.java.util;
 
 import java.math.BigDecimal;
 
+import org.junit.Test;
+
 public class ValueConvert {
 	// 人民币小写转大写
-	public static String moneySmall2Big(String cash)
-	{
+	public static String moneySmall2Big(String cash) {
 		String sUnit = "分角元拾佰仟万拾佰仟亿拾佰仟万";
 		String sNumber = "零壹贰叁肆伍陆柒捌玖";
 
@@ -58,8 +59,51 @@ public class ValueConvert {
 			return outstr;
 		}
 	}
-	
+
+	/**
+	 * @Description:四舍五入
+	 * @param num
+	 * @param i
+	 * @return
+	 */
+	public static double formatUserDefNew(double num, int i) {
+		double num_calc;
+		if (num < 0.0D) {
+			num_calc = 0.0D - num;
+		} else {
+			num_calc = num;
+		}
+
+		long ratio = (long) Math.pow(10.0D, i + 1);
+		double num_new = new BigDecimal(num_calc + "")
+				.multiply(new BigDecimal(ratio + "")).add(new BigDecimal("5"))
+				.doubleValue();
+
+		long cut = (long) num_new;
+		cut /= 10L;
+
+		double res = cut / Math.pow(10.0D, i);
+
+		if (num < 0.0D) {
+			res = 0.0D - res;
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(moneySmall2Big("12345609"));
+	}
+	
+	@Test
+	public void testFormatUserDefNew()
+	{
+		System.out.println(formatUserDefNew(173.4341, 2));
+		System.out.println(formatUserDefNew(173.4351, 2));
+		
+		System.out.println(formatUserDefNew(173.4341, 2)*100/10);
+		System.out.println(formatUserDefNew(173.4351, 2)*100/10);
+		
+		System.out.println(formatUserDefNew(17.515, 2));
+		System.out.println(formatUserDefNew(17.515, 2)*100/10);
 	}
 }
