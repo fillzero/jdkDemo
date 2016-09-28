44f93289eb4cdc8bae5c4fd8cc10009c35d127cb
lijinlong <1029171429@qq.com>
2016-09-28 18:22:05 +0800

    addTwoNumbers

---
+++ src/main/java/cn/tk/java/algorithm/leetcode/AddTwoNumbers.java
@@ -0,0 +1,46 @@
+package cn.tk.java.algorithm.leetcode;
+
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    	int carry = 0;
+    	ListNode newHead = new ListNode(0);
+    	ListNode p1 = l1, p2 = l2, p3 = newHead;
+    	while(p1!=null || p2!=null)
+    	{
+    		if (p1!=null) {
+				carry += p1.val;
+				p1 = p1.next;
+			}
+    		
+    		if (p2!=null) {
+				carry += p2.val;
+				p2 = p2.next;
+			}
+    		
+    		p3.next = new ListNode(carry%10);
+    		p3 = p3.next;
+    		carry /= 10;
+    	}
+    	
+    	if (carry==1)
+			p3.next = new ListNode(1);
+    	
+		return newHead.next;
+    }
+    
+    //(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    public static void main(String[] args) {
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(3);
+		
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+		
+		ListNode addTwoNumbers = addTwoNumbers(l1, l2);
+		while (addTwoNumbers.next!=null) {
+			System.out.println(addTwoNumbers.val);
+		}
+	}
+}
---
+++ src/main/java/cn/tk/java/algorithm/leetcode/ListNode.java
@@ -0,0 +1,10 @@
+package cn.tk.java.algorithm.leetcode;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
