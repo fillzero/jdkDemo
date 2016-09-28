3fc111b8cf6b357c49c47a066be7e66a7819f7a9
lijinlong <1029171429@qq.com>
2016-09-28 16:55:42 +0800

    LeetCode TwoSum

---
+++ src/main/java/cn/tk/java/algorithm/leetcode/TwoSum.java
@@ -0,0 +1,27 @@
+package cn.tk.java.algorithm.leetcode;
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		int[] index = {0, 0};
+		for (int i=0; i<nums.length-1; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+				int sum = nums[i] + nums[j];
+				if (sum == target) {
+					index[0] = i;
+					index[1] = j;
+					return index;
+				}
+			}
+		}
+		return index;
+    }
+	
+	public static void main(String[] args) {
+		int[] nums =  {2, 7, 11, 15};
+		int[] twoSum = twoSum(nums, 9);
+		for(int i : twoSum)
+		{
+			System.out.println(i);
+		}
+	}
+}
