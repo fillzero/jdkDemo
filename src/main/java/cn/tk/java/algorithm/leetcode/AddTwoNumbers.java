9911403476d2850876e99666620015a4f2f464e6
lijinlong <1029171429@qq.com>
2016-10-20 23:43:28 +0800

    add Two Numbers

--- src/main/java/cn/tk/java/algorithm/leetcode/AddTwoNumbers.java
+++ src/main/java/cn/tk/java/algorithm/leetcode/AddTwoNumbers.java
@@ -1,46 +1,132 @@
 package cn.tk.java.algorithm.leetcode;
 
+import org.junit.Test;
+
 public class AddTwoNumbers {
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    	int carry = 0;
-    	ListNode newHead = new ListNode(0);
-    	ListNode p1 = l1, p2 = l2, p3 = newHead;
-    	while(p1!=null || p2!=null)
-    	{
-    		if (p1!=null) {
-				carry += p1.val;
-				p1 = p1.next;
-			}
-    		
-    		if (p2!=null) {
-				carry += p2.val;
-				p2 = p2.next;
-			}
-    		
-    		p3.next = new ListNode(carry%10);
-    		p3 = p3.next;
-    		carry /= 10;
-    	}
-    	
-    	if (carry==1)
-			p3.next = new ListNode(1);
-    	
-		return newHead.next;
-    }
-    
-    //(2 -> 4 -> 3) + (5 -> 6 -> 4)
-    public static void main(String[] args) {
-		ListNode l1 = new ListNode(2);
-		l1.next = new ListNode(4);
-		l1.next.next = new ListNode(3);
-		
-		ListNode l2 = new ListNode(5);
-		l2.next = new ListNode(6);
-		l2.next.next = new ListNode(4);
-		
-		ListNode addTwoNumbers = addTwoNumbers(l1, l2);
-		while (addTwoNumbers.next!=null) {
-			System.out.println(addTwoNumbers.val);
+	/**
+	 * @description:关键问题要记录最后一个节点, 链表插入使用尾插法, 不使用头插法
+	 */
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(-1);
+		int carry = 0;
+		ListNode prev = head;
+		for (ListNode pa = l1, pb = l2; 
+				pa != null || pb != null; 
+				pa = pa == null ? null : pa.next, 
+				pb = pb == null ? null : pb.next, 
+				prev = prev.next) {
+			int ai = pa == null ? 0 : pa.value;
+			int bi = pb == null ? 0 : pb.value;
+			int value = (ai + bi + carry) % 10; //模 10 可以算出新链表的值
+			carry = (ai + bi + carry) / 10;//除 10 可以算出每次的进位
+			prev.next = new ListNode(value);
 		}
+		if (carry > 0) {
+			prev.next = new ListNode(carry);
+		}
+		return head.next;
+	}
+	
+	public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+	       int carry =0;
+	 
+	        ListNode newHead = new ListNode(0);
+	        ListNode p1 = l1, p2 = l2, p3=newHead;
+	 
+	        while(p1 != null || p2 != null){
+	            if(p1 != null){
+	                carry += p1.value;
+	                p1 = p1.next;
+	            }
+	 
+	            if(p2 != null){
+	                carry += p2.value;
+	                p2 = p2.next;
+	            }
+	 
+	            p3.next = new ListNode(carry%10);
+	            p3 = p3.next;
+	            carry /= 10;
+	        }
+	 
+	        if(carry==1) 
+	            p3.next=new ListNode(1);
+	 
+	        return newHead.next;
+	    }
+
+	/**
+	 * @description: 尾插法创建链表, 关键要维护一个尾结点
+	 */
+	public ListNode tailAdd(int ... value) {
+		ListNode head = new ListNode(-1);//头结点
+		ListNode prev = head;//尾结点
+		for (int i = 0; i < value.length; i++) {
+			prev.next = new ListNode(value[i]);
+			prev = prev.next;
+		}
+		return head.next;
+	}
+	
+	/**
+	 * @description:头插法创建链表
+	 */
+	public ListNode headAdd(int ... value)
+	{
+		ListNode head = new ListNode(-1);
+		for(int i=0; i<value.length; i++)
+		{
+			ListNode newNode = new ListNode(value[i]);
+			newNode.next = head.next;
+			head.next = newNode;
+		}
+		return head.next;
+	}
+	
+	/**
+	 * @description:打印列表
+	 */
+	private void printList(ListNode list) {
+		while (list.next != null) {
+			System.out.print(list.value + " ");
+			list = list.next;
+		}
+		System.out.print(list.value + " ");
+	}
+
+	/**
+	 * 内部节点类
+	 */
+	public class ListNode {
+		int value;
+		ListNode next;
+		ListNode(int value) {
+			this.value = value;
+		}
+	}
+
+	// (2 -> 4 -> 3) + (5 -> 6 -> 4) --> (7, 0, 8)
+	@Test
+	public void testAddTwoNumbers(){
+		ListNode l1 = tailAdd(2, 4, 3);
+		ListNode l2 = tailAdd(5, 6, 4);
+		ListNode list = addTwoNumbers(l1, l2);
+		ListNode list2 = addTwoNumbers1(l1, l2);
+		printList(list);
+		System.out.println();
+		printList(list2);
+	}
+	
+	/**
+	 * @description:测试头插法和尾插法
+	 */
+	@Test
+	public void testAdd()
+	{
+		ListNode headList = headAdd(2, 4, 3);
+		printList(headList);
+		System.out.println();
+		ListNode tailList = tailAdd(2, 4, 3);
+		printList(tailList);
 	}
 }
