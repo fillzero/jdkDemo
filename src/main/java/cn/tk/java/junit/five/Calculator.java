71f005e71f48eeeced36c9091e01d05fcdf401eb
lijinlong <1029171429@qq.com>
2016-11-11 17:32:42 +0800

    Junit5注释: 文档 + Demo + API

--- src/main/java/cn/tk/java/junit/five/Calculator.java
+++ src/main/java/cn/tk/java/junit/five/Calculator.java
@@ -1,5 +1,19 @@
 package cn.tk.java.junit.five;
 
+/**
+ * @Author: lijl85
+ * @Title: Calculator.java
+ * @Package: cn.tk.java.junit.five
+ * @Time: 2016年11月11日下午5:28:50
+ *
+ * @Description: 
+ * https://www.ibm.com/developerworks/cn/java/j-junit5/index.html
+ * 官方指南 + Demo + API: Github
+ * 
+ * 官方指南: QuickStart,快速入门
+ * Demo: 学习编写,对应指南
+ * API: 随时查阅
+ */
 public class Calculator {
 	public int add(int operand1, int operand2) {
 		return operand1 + operand2;
