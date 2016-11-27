6888d0eaea1d490bc6280a092f376947e91524a1
lijinlong <1029171429@qq.com>
2016-11-27 15:47:38 +0800

    函数式编程

--- src/main/test/newProperty/PredicateImplTest.java
+++ src/main/test/newProperty/PredicateImplTest.java
@@ -15,17 +15,17 @@ public class PredicateImplTest {
     @Test
     public void testPredicate(){
         PredicateImpl handler = new PredicateImpl();
-        PredicateImpl.Student student1 = handler.getStudent("Ashok", "Kumar", 9.5);
-        PredicateImpl.Student student2 = handler.getStudent("Rajat","Verma", 8.0);
+        PredicateImpl.Customer customer1 = handler.getStudent("Jack", 9.5);
+        PredicateImpl.Customer customer2 = handler.getStudent("Lili", 8.0);
 
-        student1 = handler.updateStudentFee(student1,
-                student -> student.grade > 8.5,//Lambda expression for Predicate interface
-                student -> student.feeDiscount = 30.0);//Lambda expression for Consumer interface
-        student1.printFee();
+        customer1 = handler.updateStudentFee(customer1,
+                customer -> customer.grade > 8.5,//Lambda expression for Predicate interface
+                consumer -> consumer.feeDiscount = 0.3);//Lambda expression for Consumer interface
+        customer1.printFee();
 
-        student2 = updateStudentFee(student2,
-                student -> student.grade >= 8,
-                student -> student.feeDiscount = 20.0);
-        student2.printFee();
+        customer2 = updateStudentFee(customer2,
+                customer -> customer.grade >= 8,
+                consumer -> consumer.feeDiscount = 0.2);
+        customer2.printFee();
     }
 }
