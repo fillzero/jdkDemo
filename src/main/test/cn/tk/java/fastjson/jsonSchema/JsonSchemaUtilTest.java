e5c5af561fea1eb382a7e62857c67761fbd79bd3
lijinlong <1029171429@qq.com>
2017-02-08 16:40:51 +0800

    JSONSchema Items 关键字学习

--- src/main/test/cn/tk/java/fastjson/jsonSchema/JsonSchemaUtilTest.java
+++ src/main/test/cn/tk/java/fastjson/jsonSchema/JsonSchemaUtilTest.java
@@ -23,6 +23,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class JsonSchemaUtilTest {
     private static String candidateStr = "";
     private static String jsonSchema = "";
+    private static String items = "";
+    private static String itemsSchema = "";
 
     @BeforeAll
     @SneakyThrows
@@ -32,6 +34,11 @@ public class JsonSchemaUtilTest {
         String schemaPath = "./src/main/test/cn/tk/java/fastjson/jsonSchema/resource/schema.txt";
         candidateStr = FileUtils.readFileToString(new File(jsonPath), Charset.forName("UTF-8"));
         jsonSchema = FileUtils.readFileToString(new File(schemaPath), Charset.forName("UTF-8"));
+
+        String itemsPath = "./src/main/test/cn/tk/java/fastjson/jsonSchema/resource/items.json";
+        String itemsSchemaPath = "./src/main/test/cn/tk/java/fastjson/jsonSchema/resource/schema_items.txt";
+        items = FileUtils.readFileToString(new File(itemsPath), Charset.forName("UTF-8"));
+        itemsSchema = FileUtils.readFileToString(new File(itemsSchemaPath), Charset.forName("UTF-8"));
     }
 
     @Test
@@ -43,5 +50,38 @@ public class JsonSchemaUtilTest {
         System.out.println(candidateStr);
         System.out.println(jsonSchema);
         assertTrue(JsonSchemaUtil.validate(jsonSchema, candidateStr));
+    }
+
+    /*
+    * @description: 整体 type 是 array, 其中每个元素都有自己的校验规则, 由 items 设定
+    * list: 数组里面多个元素共享一个 Schema
+    * tuple: 数组里面每个元素都有自己的 Schema
+    * length: 对数组中元素的个数作出校验
+    * unique: 对数组是否允许重复元素作出校验, true: 不允许有重复元素, false: 元素有重复元素
+    */
+    @Test
+    public void validateItemList()
+    {
+        JSONObject itemsObject = JSON.parseObject(items);
+        JSONObject itemsSchemaObject = JSON.parseObject(itemsSchema);
+        String listSchema = itemsSchemaObject.getJSONObject("list").toJSONString();//
+        String tupleSchema = itemsSchemaObject.getJSONObject("tuple").toJSONString();
+        String lengthSchema = itemsSchemaObject.getJSONObject("length").toJSONString();
+        String uniqueSchema = itemsSchemaObject.getJSONObject("unique").toJSONString();
+
+        assertTrue(JsonSchemaUtil.validate(listSchema, itemsObject.getJSONArray("pass1").toJSONString()));
+        assertTrue(JsonSchemaUtil.validate(listSchema, itemsObject.getJSONArray("pass2").toJSONString()));
+        assertFalse(JsonSchemaUtil.validate(listSchema, itemsObject.getJSONArray("fail1").toJSONString()));
+
+        assertTrue(JsonSchemaUtil.validate(tupleSchema, itemsObject.getJSONArray("pass3").toJSONString()));
+        assertTrue(JsonSchemaUtil.validate(tupleSchema, itemsObject.getJSONArray("pass4").toJSONString()));
+        assertFalse(JsonSchemaUtil.validate(tupleSchema, itemsObject.getJSONArray("fail2").toJSONString()));
+
+        assertTrue(JsonSchemaUtil.validate(lengthSchema, itemsObject.getJSONArray("pass5").toJSONString()));
+        assertTrue(JsonSchemaUtil.validate(lengthSchema, itemsObject.getJSONArray("pass6").toJSONString()));
+        assertFalse(JsonSchemaUtil.validate(lengthSchema, itemsObject.getJSONArray("fail3").toJSONString()));
+
+        assertTrue(JsonSchemaUtil.validate(uniqueSchema, itemsObject.getJSONArray("pass7").toJSONString()));
+        assertFalse(JsonSchemaUtil.validate(uniqueSchema, itemsObject.getJSONArray("fail4").toJSONString()));
     }
 }
