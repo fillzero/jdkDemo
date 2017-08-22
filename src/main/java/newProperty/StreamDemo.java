f6adeb90ed579f2dd1cabe262cfd3a36d53b3cac
lijinlong <1029171429@qq.com>
2017-08-22 19:23:05 +0800

    函数式编程思维

--- src/main/java/newProperty/StreamDemo.java
+++ src/main/java/newProperty/StreamDemo.java
@@ -1,7 +1,11 @@
 package newProperty;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -24,5 +28,43 @@ public class StreamDemo {
 		{
 			System.out.print(iterator.next() + ",");
 		}
+	}
+
+	@Test
+	public void testOf()
+	{
+		String[] names = {"wenchao", "liujia", "lijinlong", "shanling"};
+		Stream<String> stream = Stream.of(names);//转成 Stream
+		stream.forEach(System.out::println);
+	}
+
+	/**
+	 * Description：Stream 的中间操作
+	 */
+	@Test
+	public void testMiddleOperate()
+	{
+		List<Integer> list = Arrays.asList(1, 2, 3, 8, 4, 5, 6, 7, 8, 9, 10);
+		list.stream()
+				.filter(a -> a % 2 == 0)
+				.distinct()
+				.sorted((a, b) -> b - a)
+				.skip(2)
+				.limit(3)
+				.forEach(System.out::println);
+	}
+
+	/**
+	 * Description：返回一个 Map
+	 */
+	@Test
+	public void testMap()
+	{
+		List<Integer> list = Arrays.asList(1, 2, 3, 8, 4, 5, 6, 7, 8, 9, 10);
+		Map<Integer, Integer> map = list.stream()
+				.map(a -> a * 2)
+				.distinct()
+				.collect(Collectors.toMap(i1 -> i1, i1 -> i1));
+		System.out.println(map);
 	}
 }
