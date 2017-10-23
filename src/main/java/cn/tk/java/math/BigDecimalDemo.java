8f45007fed713d2cda3279e502a93c1d38fff93b
lijinlong <1029171429@qq.com>
2017-10-23 17:16:38 +0800

    BigDecimal 舍入模式梳理

--- src/main/java/cn/tk/java/math/BigDecimalDemo.java
+++ src/main/java/cn/tk/java/math/BigDecimalDemo.java
@@ -63,4 +63,28 @@ public class BigDecimalDemo {
 	    long lPremium2 = bigDecimal2.longValue();
 	    System.out.println("保费(单位/分): " + lPremium2);
 	}
+
+	/**
+	 * Description: BigDecimal 舍入模式分析
+	 */
+	@Test
+	public void testRoundingMode()
+	{
+		double number = 2.45;
+		BigDecimal roundUp = new BigDecimal(number).setScale(1, BigDecimal.ROUND_UP);//远离0方向舍入
+		BigDecimal roundDown = new BigDecimal(number).setScale(1, BigDecimal.ROUND_DOWN);//靠近0方向
+		BigDecimal roundCeiling = new BigDecimal(number).setScale(1, BigDecimal.ROUND_CEILING);//向上取整（正无穷大）
+		BigDecimal roundFloor = new BigDecimal(number).setScale(1, BigDecimal.ROUND_FLOOR);//向下取整（负无穷大）
+		BigDecimal roundHalfUp = new BigDecimal(number).setScale(1, BigDecimal.ROUND_HALF_UP);//向距离最接近数字舍入，相等的话向上舍入
+		BigDecimal roundHalfDown = new BigDecimal(number).setScale(1, BigDecimal.ROUND_HALF_DOWN);//向距离最接近数字舍入，相等的话向下舍入
+		BigDecimal roundHalfEven = new BigDecimal(number).setScale(1, BigDecimal.ROUND_HALF_EVEN);//向距离最接近数字舍入，相等的话向偶数舍入
+
+		System.out.println(roundUp);
+		System.out.println(roundDown);
+		System.out.println(roundCeiling);
+		System.out.println(roundFloor);
+		System.out.println(roundHalfUp);
+		System.out.println(roundHalfDown);
+		System.out.println(roundHalfEven);
+	}
 }
